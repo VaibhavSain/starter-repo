5faa7dd0ee9276e524632b7065de5c95c933fb8d
Vaibhav Sain <155076216+VaibhavSain@users.noreply.github.com>
2024-09-15 21:24:32 +0530

    Add files via upload

---
+++ starter repo.java
@@ -0,0 +1,51 @@
+package com.example.demo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+
+@Entity
+public class ExampleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private Integer age;
+
+    // Default constructor
+    public ExampleEntity() {}
+
+    // Constructor with all parameters
+    public ExampleEntity(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+}
